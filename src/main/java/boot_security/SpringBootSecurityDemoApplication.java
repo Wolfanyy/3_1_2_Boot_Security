2954334ec5a949e48951a3f439760b196bfeabc3
Wolfanyy <gribanova.anna98@mail.ru>
2025-11-21 23:10:58 +0400

    feat: add initial Spring Security configuration and login success handler

---
+++ src/main/java/boot_security/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+    }
+}
