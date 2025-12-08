672686ea475a86a912b95cf7f5b8a63a5e89a1e7
Wolfanyy <gribanova.anna98@mail.ru>
2025-12-08 21:32:58 +0400

    feat: implement UserDetailsService to load users by email

--- src/main/java/boot_security/dao/UserDao.java
+++ src/main/java/boot_security/dao/UserDao.java
@@ -3,5 +3,8 @@ package boot_security.dao;
 import boot_security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserDao extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
