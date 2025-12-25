b2d8b09c530237a92599e0b7af544000c2dc4c6d
Wolfanyy <gribanova.anna98@mail.ru>
2025-12-25 08:38:05 +0400

    fix: update authManager bean and add logoutSuccessUrl

--- src/main/java/boot_security/config/SecurityConfig.java
+++ src/main/java/boot_security/config/SecurityConfig.java
@@ -5,17 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -41,8 +36,10 @@ public class SecurityConfig {
                         .permitAll()
 
                 )
-                .logout(LogoutConfigurer::permitAll)
-                .userDetailsService(userDetailsService);
+                .logout(logout -> logout
+                        .permitAll()
+                        .logoutSuccessUrl("/")
+                );
 
         return http.build();
     }
@@ -53,7 +50,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authManager(HttpSecurity http) {
-        return http.getSharedObject(AuthenticationManager.class);
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
